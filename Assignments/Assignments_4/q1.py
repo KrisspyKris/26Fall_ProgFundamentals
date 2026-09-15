28c492eae6762cc02430beb1e32b32d4bd936539
Kris Falkner <cfalkner@twu.edu>
2026-09-15 11:57:35 -0500

    Updated first assignment

--- Assignments/Assignments_4/q1.py
+++ Assignments/Assignments_4/q1.py
@@ -4,6 +4,6 @@ age = 21
 meters = 1.75 #in meters
 
 feet = math.floor(meters * 3.281)
-inches = round((meters * 39.37) % 12)
+inches = math.floor((meters * 39.37) % 12)
 
 print(f"{name} is {age} years old and {meters} meters tall, or in good ol' murican units, {feet} foot {inches} inches.")
